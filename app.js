7800cba729125165a036cbdbd0095ec8b06c7917
dev1654 <104884276+dev1654@users.noreply.github.com>
2025-10-29 19:30:38 -0400

    fixing css

--- app.js
+++ app.js
@@ -18,8 +18,7 @@ const PORT = process.env.PORT || 3000;
 // Middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static('public'));
-
+app.use(express.static(path.join(__dirname, 'public')));
 // Configure Handlebars
 app.engine('.hbs', engine({
     extname: '.hbs',
